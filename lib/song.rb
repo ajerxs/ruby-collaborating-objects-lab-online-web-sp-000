961905d11161ac253cdcfcc00de766144108bf31
Andrew Eames <ajerxs@gmail.com>
2020-12-01 00:55:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,8 +17,13 @@ class Song
     song_name = file.split(" - ")[1]
     artist = file.split(" - ")[0]
     song = self.new(song_name)
-    song.artist = artist
+    song.artist_name = artist
     song
+  end
+
+  def artist_name(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
   end
 
 end
